ea986f6dedacfacc42b1141606eeb78de87d560d
galguss <101664102+galguss@users.noreply.github.com>
2022-12-07 08:59:52 +0200

    Add files via upload

--- Ktechedge/server/client/src/components/routes/GetUsers.jsx
+++ Ktechedge/server/client/src/components/routes/GetUsers.jsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import DataTable from 'react-data-table-component';
+import '../../styles/routesStyle/getUsers.css';
 
 function GetUsers(){
-    return <h1>Get users!</h1>
+    const [users, setUsers] = useState([{}]);
+
+    const colums = [
+        {
+            name: 'user_id',
+            selector: (row) => row.user_id
+        },
+        {
+            name: 'email',
+            selector: (row) => row.email
+        },
+        {
+            name: 'password',
+            selector: (row) => row.password
+        },
+        {
+            name: 'github',
+            selector: (row) => row.github
+        },
+        {
+            name: 'full_name',
+            selector: (row) => row.full_name
+        },
+        {
+            name: 'image',
+            selector: (row) => row.image
+        },
+        {
+            name: 'level',
+            selector: (row) => row.level
+        }
+    ];
+    
+    useEffect(() => fetchUsersData, []);
+
+    async function fetchUsersData(){
+        const URL = 'http://localhost:3050/admin';
+        const response = await fetch(URL);
+
+        const users = await response.json();
+        setUsers(users);
+    }
+
+    
+   
+
+    return(
+       
+            <div id='userTable'>
+            <DataTable 
+            title ='Users'
+            columns={colums}
+            data ={users}
+            />
+            </div>       
+    )
 }
 
 export default GetUsers;
